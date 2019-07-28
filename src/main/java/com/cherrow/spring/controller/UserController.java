67ab2181c0fc0098c631845f30aa5682a66ca317
cherrow <cherrow@foxmail.com>
2019-07-28 18:08:11 +0800

    add api to add user

--- src/main/java/com/cherrow/spring/controller/UserController.java
+++ src/main/java/com/cherrow/spring/controller/UserController.java
@@ -1,14 +1,25 @@
 package com.cherrow.spring.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.cherrow.spring.domain.User;
+import com.cherrow.spring.service.UserService;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
 
-    @GetMapping("/hello")
-    public String sayHello(){
-        return "hello, red";
+    private UserService userService;
+
+    public UserController(UserService userService){
+        this.userService = userService;
     }
 
+    @GetMapping("/hello")
+    public String sayHello(){
+        return userService.sayHello();
+    }
+
+    @PostMapping("/new-user")
+    public User addUser(@RequestParam String name, @RequestParam int age){
+        return userService.createUser(name, age);
+    }
 }
