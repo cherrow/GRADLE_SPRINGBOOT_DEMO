449906b0960ff41cf2d45b89999e6ea2b2029460
cherrow <cherrow@foxmail.com>
2019-07-28 22:06:22 +0800

    override method equals

--- src/main/java/com/cherrow/spring/domain/User.java
+++ src/main/java/com/cherrow/spring/domain/User.java
@@ -11,7 +11,17 @@ public class User {
     private String name;
     private int age;
 
-    public String introduceSelf(){
-        return "Hello, my name is "+ name + ", I'm "+ age +" years old";
+    public String introduceSelf() {
+        return "Hello, my name is " + name + ", I'm " + age + " years old";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof User) {
+            User person = (User) obj;
+            return name.equalsIgnoreCase(person.getName().trim())
+                    && age == person.getAge();
+        }
+        return false;
     }
 }
