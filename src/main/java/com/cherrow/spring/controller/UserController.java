67a8728b28b91ba8bf1277a09b35b07909de649f
cherrow <cherrow@foxmail.com>
2019-07-30 21:55:14 +0800

    update autowired constructor

--- src/main/java/com/cherrow/spring/controller/UserController.java
+++ src/main/java/com/cherrow/spring/controller/UserController.java
@@ -2,13 +2,15 @@ package com.cherrow.spring.controller;
 
 import com.cherrow.spring.domain.User;
 import com.cherrow.spring.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
 
-    private UserService userService;
+    private final UserService userService;
 
+    @Autowired
     public UserController(UserService userService){
         this.userService = userService;
     }
